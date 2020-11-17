609406cc3d404498d5dea77f3bfd613f64e77b90
viktor <viktorkarlsson125@gmail.com>
2020-11-17 16:41:42 +0100

    adding and activation rout. testing api with rspec. creating pings controller

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,5 @@
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'spec_helper'
@@ -14,5 +14,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include FactoryBot::Syntax::Methods
-  config.include(Shoulda::Matchers::ActiveReacord, type: :model)
+  config.include(Shoulda::Matchers::ActiveRecord, type: :model)
 end
