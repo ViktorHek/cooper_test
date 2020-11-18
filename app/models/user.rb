b64a25f2ca9ff7d65d5a082e6f36c9643b848f6c
viktor <viktorkarlsson125@gmail.com>
2020-11-18 13:04:15 +0100

    adding test and PerformanceData. but test fails with syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
-
-
 class User < ActiveRecord::Base
   extend Devise::Models
+
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+    has_many :performance_data, class_name: 'PerformenceData'
 end
